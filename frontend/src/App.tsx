e67ecf484507f589149ee345c72358ed6c767ec4
jw122 <thejuliawu@gmail.com>
2021-07-04 10:35:58 -0700

    update route name

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -29,7 +29,7 @@ class App extends Component {
             <Landing />
           </Route>
           {/* TODO: update to no longer hard code when user management is in place */}
-          <Route path="/satoshi">
+          <Route path="/shibainu">
             <MerchantProfile />
           </Route>
           <Route path="/dashboard">
