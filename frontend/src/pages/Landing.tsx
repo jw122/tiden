0f7b133ec786c4ecbf35d150c09df4977e38a18a
jw122 <thejuliawu@gmail.com>
2021-07-08 15:49:52 -0700

    landing page

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,40 +1,49 @@
 import logo from "../logo.svg";
-import {Col, Container, Row, Image, Button} from "react-bootstrap";
-import { useHistory } from 'react-router-dom';
+import { Col, Container, Row, Image, Button } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
 
 import "./Landing.css";
 
-
 const Landing = () => {
+  const history = useHistory();
+  const handleClick = () => history.push("/sign-up");
 
-    const history = useHistory();
-    const handleClick = () => history.push('/sign-up');
+  return (
+    <div>
+      <Container className="Landing-main-container">
+        <embed src={logo} className="App-logo" />
+        <Row className="Landing-main-container">
+          <h1>Seamless payments with credit cards and crypto</h1>
+          <br />
+          <h5>
+            Take payments with credit cards or crypto, and earn interest on your
+            balances immediately.
+          </h5>
+        </Row>
 
-    return (
-        <div>
-            <Container className="Landing-main-container">
-                <Row className="Landing-main-container">
-                    <embed src={logo} className="App-logo"/>
-                    <h1>Easy payments with credit cards and crypto</h1>
-                    Take payments from credit cards or crypto, and earn interest on your balances.
-                </Row>
-                <br/><br/><br/>
-                <Row>
-                    <Button onClick={handleClick}> Start earning now </Button>
-                </Row>
-                <br/><br/><br/>
-                <Row>
-                    <Col sm>
-                        <Image src={process.env.PUBLIC_URL + '/img/screenshot_shibainu.png'} fluid />
-                    </Col>
-                    <Col sm>
-                        <Image src={process.env.PUBLIC_URL + '/img/screenshot_merchant_dash.png'} fluid />
-                    </Col>
-                </Row>
+        <Row className="cta-button-row">
+          <Button variant="warning" size="lg" onClick={handleClick}>
+            Start Earning Now
+          </Button>
+        </Row>
 
-            </Container>
-        </div>
-    );
-}
+        <Row>
+          <Col sm>
+            <Image
+              src={process.env.PUBLIC_URL + "/img/screenshot_shibainu.png"}
+              fluid
+            />
+          </Col>
+          <Col sm>
+            <Image
+              src={process.env.PUBLIC_URL + "/img/screenshot_merchant_dash.png"}
+              fluid
+            />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
 export default Landing;
